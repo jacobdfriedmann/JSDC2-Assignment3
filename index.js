f8766e9da93f2cc9fea6b264fbe0191eb6144dfd
Jacob Friedmann <jacobdfriedmann@gmail.com>
2016-06-30 17:47:44 -0400

    Add questions

---
+++ index.js
@@ -0,0 +1,62 @@
+/**
+ * Q0: Write a function called `countdown` that counts down from 10 on the console and
+ * then logs "Blast Off!"
+ */
+
+
+/**
+ * Q1: Write a function called `max` that has two Number parameters and returns
+ * the larger of the two.
+ */
+
+
+/**
+ * Q2: Write a function called `longer` that has two String parameters and returns
+ * the String that is longer.
+ */
+
+
+/**
+ * Q3: Write a function called `isEven` that has one Number parameter and returns `true` if
+ * if the number is even and `false` otherwise.
+ */
+
+ /**
+  * Q4: Write a function called `getAreaOfCircle` that has a Number parameter
+  * (the circle's radius) and returns the surface area.
+  */
+
+
+/**
+ * Q5: Write a function called `getFullName` that has an Object parameter, of the form:
+ *
+ * {
+ *   firstName: 'Bob',
+ *   lastName: 'Evans'
+ * }
+ *
+ * And returns the first and last name concatenated together (i.e. 'Bob Evans').
+ */
+
+
+/**
+ * Q6: Write a function called `reverseString` that has a String parameter and returns
+ * the String reversed. Hint: look up `.split()`
+ */
+
+
+/**
+ * Q7: Write a function called `maxArray` that has one Array of Numbers parameter and returns
+ * the largest number in the Array. Use a `forEach` loop.
+ */
+
+
+/**
+ * Q8: Create an Object that has each of the above functions as values.
+ *
+ * var answers = {
+ *   q1: ?,
+ *   ...
+ * };
+ * 
+ */
