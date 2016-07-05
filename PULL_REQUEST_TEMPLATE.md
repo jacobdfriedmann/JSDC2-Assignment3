14343df08b4ae39dce8a476f4067d79450a4ae9a
Nathan Keeyss <keeysnc@gmail.com>
2016-07-05 00:37:22 -0400

    Assignment 3 Attempt

--- PULL_REQUEST_TEMPLATE.md
+++ PULL_REQUEST_TEMPLATE.md
@@ -1,13 +1,15 @@
-# [Your Name] Homework Submission
+# Nathan Keeys Homework Submission
 
 ## Temperature Check
 
-Rate on a scale of 1-10.
+Rate on a scale of 1-10. ---5
 
-Comfort:
+Comfort: --- 5
 
-Completeness:
+Completeness: --- 5/9
 
 ## Questions?
 
-What questions do you have? What would like us to take a close look at when reviewing your HW?
+1. I'm having trouble grasping the concept behind a forEach loop. Specifically what it is and what the difference is from the .forEach method.
+
+2. I am also not fully understanding returning string parameters and object parameters.
--- index.js
+++ index.js
@@ -3,11 +3,34 @@
  * then logs "Blast Off!"
  */
 
+function countdown(){
+
+while(i > 1){
+
+	i--;
+	console.log(i);
+
+	if(i === 1){
+
+		console.log('blast off!');
+	}
+}
+
+}
+
 
 /**
  * Q1: Write a function called `max` that has two Number parameters and returns
  * the larger of the two.
  */
+
+function max(greater, less){
+
+	return Math.max(greater, less);
+
+}
+
+console.log(max(5 , 3));
 
 
 /**
@@ -16,15 +39,44 @@
  */
 
 
+
+
+
 /**
  * Q3: Write a function called `isEven` that has one Number parameter and returns `true` if
  * if the number is even and `false` otherwise.
  */
 
+function isEven(num){
+
+	if(num % 2 === 0){
+
+		console.log('true')
+	}else{
+
+		console.log('false')
+	}
+
+}
+
+isEven(33);
+
+
+
+
  /**
   * Q4: Write a function called `getAreaOfCircle` that has a Number parameter
   * (the circle's radius) and returns the surface area.
   */
+
+function getAreaOfCircle(distance){
+
+	radius = distance/2;
+	console.log(radius);
+
+}
+
+getAreaOfCircle(13);
 
 
 /**
@@ -39,16 +91,64 @@
  */
 
 
+/*
+function getFullName({ 
+
+	 firstName : 'Bob', 
+	 lastName : 'Evans' 
+}){
+
+	firstName + lastName
+
+	};
+
+
+console.log(getFullName);
+
+*/
+
+
+
+
 /**
  * Q6: Write a function called `reverseString` that has a String parameter and returns
  * the String reversed. Hint: look up `.split()`
  */
 
 
+
+
+function reverseString( contentReversed ){
+
+	return contentReversed.split('').reverse().join('');
+}
+
+console.log(reverseString('This string will be reversed'));
+
+/**
+
 /**
  * Q7: Write a function called `maxArray` that has one Array of Numbers parameter and returns
  * the largest number in the Array. Use a `forEach` loop.
- */
+
+/*
+var myArray = ['1', '2', '3'];
+
+function maxArray(arr, func) {
+  for (var i = 0; i < arr.length; i++) {
+
+   func(arr[i]);
+
+  }
+}
+
+
+	function speak(word) {
+	  console.log(word);
+	}
+
+speak();
+*/
 
 
 /**
@@ -59,6 +159,20 @@
  *   ...
  * };
  *
+ */
+
+ /*
+
+ var answers = {
+
+ 	q0: countdown(),
+
+	q1:
+
+
+
+ };
+
  */
 
  /**
